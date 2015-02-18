41c012c1ffe1c9497681dd2810684246af7e0417
Fumio SAGAWA <fumio.sagawa@gmail.com>
2015-02-18 12:17:45 +0900

    サイドバーを有効にした

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,16 @@
  * Controller of the ngJapan
  */
 angular.module('ngJapan')
-  .controller('AppCtrl', function ($scope) {
-
-    $scope.awesomeThings = [];
-  });
+  .controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+  $scope.section = [
+    {name: 'About'},
+    {name: 'Speakers'},
+    {name: 'Schedule'},
+    {name: 'Tickets'},
+    {name: 'Getting There'},
+    {name: 'Sponsors'}
+  ];
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+}]);
