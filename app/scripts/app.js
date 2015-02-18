56f6246d89cd233894f419628c2874494bdc6644
Fumio SAGAWA <fumio.sagawa@gmail.com>
2015-02-18 10:29:43 +0900

    仮サイト作成

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name AppCtrl
+ * @description
+ * # AppCtrl
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ngJapan', [
+    'ngAnimate',
+    'ngAria',
+    'ngMaterial'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngJapanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngJapanApp
+ */
+angular.module('ngJapan')
+  .controller('AppCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
